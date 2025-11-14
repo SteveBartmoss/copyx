3d7ce1de4a7376722d72635032c9924c18550944
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-11-13 21:40:28 -0600

    feat: Se implemento un archivo de cabeceras

---
+++ copyx.h
@@ -0,0 +1,12 @@
+#ifndef COPYX
+#define COPYX
+
+int copy_file(const char *src, const char *dst)
+
+int copy_file_fast(const char *src, const char *dst)
+
+int copy_file_fast_range(const char *src, const char *dst)
+
+int copy_strategy( const char *src, const char *dst)
+
+#endif 
